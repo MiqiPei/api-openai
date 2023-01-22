069b61796f9a1f7623ec0a2def3fd5974a42c62b
MinqiPei <peiminqi@gmail.com>
2023-01-22 00:02:54 -0800

    3 questions

--- pages/api/openai1.js
+++ pages/api/openai1.js
@@ -2,18 +2,18 @@ const OpenAI = require('openai-api');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 export default async (req, res) => {
-  let prompt = `middle school subjects related to ${req.search}:`;
-  const gptResponse = await openai.complete({
-    engine: 'davinci',
-    prompt: prompt,
-    maxTokens: 50,
-    temperature: 0,
-    topP: 1,
-    presencePenalty: 0.3,
-    frequencyPenalty: 0.3,
-    bestOf: 1,
-    n: 1
-});
+    let prompt = `List 5 school subjects related to ${req.search}:`;
+    const gptResponse = await openai.complete({
+      engine: 'text-davinci-003',
+      prompt: prompt,
+      maxTokens: 200,
+      temperature: 0.5,
+      topP: 1,
+      presencePenalty: 0.5,
+      frequencyPenalty: 0.52,
+      bestOf: 1,
+      n: 1
+  });
 
   res.status(200).json({text: `${gptResponse.data.choices[0].text}`})
 }
